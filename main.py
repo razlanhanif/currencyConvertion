d5dbc5941a137cdf5c98fe81ea093eb219cef59f
Hanif <razlanhanif1@gmail.com>
2023-07-18 12:30:37 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("This is a currency converter")
+
+    lira = eval(input("Enter amount in lira: "))
+    myr = 0.17
+
+    print("The total amount of for lira:" + str(lira) + " is RM:" + str(myr * lira))
+
+
+main()
